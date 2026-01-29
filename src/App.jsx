4d7b1c2fe20bdaae322df2a0edf2e4e2efc3bf7b
Danilazio1992 <danilazio92@gmail.com>
2026-01-29 17:23:05 +0100

    ho aggiustato city card funzione hovered

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,50 @@
-import { useState } from 'react'
-import './App.css'
-import CreateCardForm from './Components/CreateCardForm'
-import CityCard from './Components/CityCard'
-import {cities} from './assets/cities'
-import TopBar from './Components/TopBar'
-import Modal from './Components/Modal'
-import Footer from './Components/Footer'
-
+import { useState } from "react";
+import "./App.css";
+import { cities } from "./assets/cities";
+import CityCard from "./Components/CityCard";
+import Footer from "./Components/Footer";
+import Modal from "./Components/Modal";
+import TopBar from "./Components/TopBar";
 
 function App() {
-  const [newData, setNewData] = useState(cities)
-  const [idhovered,setIdHovered] =useState(-1) 
-  const [hovered,setHovered] = useState(false)
-  const [isModal,setIsModal] = useState(false)
-  
+  const [newData, setNewData] = useState(cities);
+  const [idHovered, setIdHovered] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [isModal, setIsModal] = useState(false);
 
-
-
-/* const a = ((e) =>{ 
+  /* const a = ((e) =>{ 
   setHovered(!hovered)
   setIdHovered(e.currentTarget.id)
   console.log(`idHovered: ${idhovered}`)
   console.log(`event target: ${e.currentTarget.id}`)
 }) */
-  const handleRemove = (id) => {setNewData(prevData => prevData.filter(el => el.id !== id))}
-
+  const handleRemove = (id) => {
+    setNewData((prevData) => prevData.filter((el) => el.id !== id));
+  };
 
   return (
-    <div className='flex w-full h-full flex-col bg-[#fae5cdc7]'>
-    {isModal && <Modal setIsModal={setIsModal} setNewData={setNewData}/>}
-    <TopBar setIsModal={setIsModal} />
-      <section className=''>
-        <div className='flex flex-wrap gap-4 p-4 w-full justify-center '>
-      {newData.map((city) => (
-        <CityCard city={city} key={city.id} handleRemove={handleRemove} hoverd={hovered} setHovered={setHovered} idhovered={idhovered} setIdHovered={setIdHovered} />))} 
+    <div className="flex w-full h-full flex-col bg-[#fae5cdc7]">
+      {isModal && <Modal setIsModal={setIsModal} setNewData={setNewData} />}
+      <TopBar setIsModal={setIsModal} />
+      <section className="">
+        <div className="flex flex-wrap gap-4 p-4 w-full justify-center ">
+          {newData.map((city) => (
+            <CityCard
+              city={city}
+              key={city.id}
+              handleRemove={handleRemove}
+              hovered={hovered}
+              setHovered={setHovered}
+              idHovered={idHovered}
+              setIdHovered={setIdHovered}
+            />
+          ))}
         </div>
       </section>
-       <div className='flex'>
-      </div> 
+      <div className="flex"></div>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
