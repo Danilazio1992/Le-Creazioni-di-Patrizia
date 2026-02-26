c4e4439f4cd691f4022df78325ca7565b546e702
Daniele <danilazio92@gmail.com>
2026-02-26 09:15:43 +0100

    ho aggiunto e appreso bene il customHook context con tanto di appunti per rileggere

--- src/AuthContext/authContext.jsx
+++ src/AuthContext/authContext.jsx
@@ -1,6 +1,6 @@
-import { createContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 import authInitialState from "./authInitialState";
-import { authReducer } from "./authReducer";
+import { authReducer } from "./AuthReducer";
 
 const authContext = createContext(null); // null è il valore inizilializzato che decidiamo di dargli
 
@@ -9,11 +9,30 @@ const logToken = "logToken";
 //passiamo children per far si che tutto cio che wrappa viene riportato così come e che i child possano accedere al context
 export function authProvider({ children }) {
   const [state, dispatch] = useReducer(authInitialState, authReducer);
+
+  const value = {
+    state,
+    dispatch,
+  };
+
   return (
     <authContext.Provider value={value}>
-      {" "}
       {/* proprietà value è l'oggetto che passa il a tutti i child wrappati e che ne hanno accesso*/}
       {children} {/* contenuto che viene wrappato */}
     </authContext.Provider>
   );
 }
+
+export const useAuth = () => {
+  const context = useContext(authContext); // gli context è legato authContext
+  if (!context) throw new Error("useAuth va utilizzato dentro al contesto AuthProvider");
+  return context 
+}
+
+/* 
+esempio di utilizzo
+
+lo importo dentro il componente che mi interessa destrutturazione
+  const{state,dispatch} = useAuth() //sarebbe value (variabile) alla quale abbiamo assegnato quello che abbiamo ricavato dal reducer (state,dispatch)
+  cosi che il componente possa accedere direttamente al reducer e lo stato del contesto
+*/
