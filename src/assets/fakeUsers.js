a3485d27213c04bbd19cc9cb9c76ca60f58adedd
Daniele Tentella <tnt_11@MacBook-Air-di-Daniele.local>
2026-03-05 08:15:02 +0100

    roba login

---
+++ src/assets/fakeUsers.js
@@ -0,0 +1,34 @@
+export const fakeUsers = [
+  {
+    id: 1,
+    name: "Daniele Tentella",
+    email: "daniele@test.com",
+    password: "123456",
+    role: "admin",
+    avatar: "https://i.pravatar.cc/150?img=3",
+  },
+  {
+    id: 2,
+    name: "Marco Rossi",
+    email: "marco@test.com",
+    password: "password",
+    role: "user",
+    avatar: "https://i.pravatar.cc/150?img=8",
+  },
+  {
+    id: 3,
+    name: "Giulia Bianchi",
+    email: "giulia@test.com",
+    password: "qwerty",
+    role: "user",
+    avatar: "https://i.pravatar.cc/150?img=5",
+  },
+  {
+    id: 4,
+    name: "Luca Verdi",
+    email: "luca@test.com",
+    password: "test123",
+    role: "editor",
+    avatar: "https://i.pravatar.cc/150?img=12",
+  },
+];
