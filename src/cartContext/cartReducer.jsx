471f46d6ff0db2062807f50bd0701c2914182e02
Daniele <danilazio92@gmail.com>
2026-03-26 13:54:44 +0100

    cambiato poco e niente

--- src/cartContext/cartReducer.jsx
+++ src/cartContext/cartReducer.jsx
@@ -1,7 +1,6 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      console.log(action.payload);
       return {
         ...state,
         products: [...state.products, action.payload],
